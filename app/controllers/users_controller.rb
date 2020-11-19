6dfe68b90c71271d4b98e2c80ab5e506f801c135
miho.furukawa <homiwakarufu@gmail.com>
2020-11-19 14:05:27 +0000

    favorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :like]
 
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
@@ -9,6 +9,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @microposts = @user.microposts.order(id: :desc).page(params[:page])
     counts(@user)
+    
+    
   end
 
   def new
@@ -38,6 +40,13 @@ class UsersController < ApplicationController
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
+  
+  def likes
+    @user = User.find(params[:id])
+    @likes = @user.likes.order(id: :desc).page(params[:page])
+    counts(@user)
+    
+  end  
   
   private
   
